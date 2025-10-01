a8089a6d8aafd800b905e668059ecb35e874f00d
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2025-10-01 13:57:57 +0200

    Working merge with accordion

--- tentacle_try1/README.md
+++ tentacle_try1/README.md
@@ -1,1 +1,4 @@
 https://pyscript.com/@examples/simple-panel
+
+https://panel.holoviz.org/reference/layouts/Accordion.html
+
--- tentacle_try1/main.py
+++ tentacle_try1/main.py
@@ -1,4 +1,5 @@
 import panel as pn
+from bokeh.plotting import figure
 
 pn.extension(sizing_mode="stretch_width")
 
@@ -8,3 +9,17 @@ def callback(new):
     return f'Amplitude is: {new}'
 
 pn.Row(slider, pn.bind(callback, slider)).servable(target='simple_app')
+
+
+p1 = figure(width=300, height=300, name='Scatter', margin=5)
+p1.scatter([0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 2, 1, 0])
+
+p2 = figure(width=300, height=300, name='Line', margin=5)
+p2.line([0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 2, 1, 0])
+
+accordion = pn.Accordion(('Scatter', p1), p2)
+p3 = figure(width=300, height=300, name='Square', margin=5)
+p3.scatter([0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 2, 1, 0], marker='square', size=10)
+
+accordion.append(p3)
+pn.Row(accordion).servable(target='accordion')
