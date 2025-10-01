ba3fe60dfe48438770b49b4f6c1f64f30d342675
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2025-10-01 14:15:24 +0200

    tmp

--- tentacle_try1/main.py
+++ tentacle_try1/main.py
@@ -1,5 +1,4 @@
 import panel as pn
-from bokeh.plotting import figure
 
 pn.extension(sizing_mode="stretch_width")
 
@@ -11,14 +10,10 @@ def callback(new):
 pn.Row(slider, pn.bind(callback, slider)).servable(target='simple_app')
 
 
-p1 = figure(width=300, height=300, name='Scatter', margin=5)
-p1.scatter([0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 2, 1, 0])
+p1 = pn.widgets.Button( name='Scatter', margin=5)
 
-p2 = figure(width=300, height=300, name='Line', margin=5)
-p2.line([0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 2, 1, 0])
+p2 = pn.widgets.Button( name='Line', margin=5)
 
-p3 = figure(width=300, height=300, name='Square', margin=5)
-p3.scatter([0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 2, 1, 0], marker='square', size=10)
-
+p3 = pn.pane.SVG('https://assets.holoviz.org/panel/samples/svg_sample2.svg', name='Square', sizing_mode='stretch_width')
 pn.Accordion(p1, p2, p3, toggle=True, header_color='green').servable(target='accordion')
 
